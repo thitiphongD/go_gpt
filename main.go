e6e55cbf200a66f6792748b140cb97f746410f98
thitiphongD <thitiphong.srisavat@gmail.com>
2023-02-16 16:32:27 +0700

    connect database

--- main.go
+++ main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	r := gin.Default()
+	db, err := sql.Open("mysql", "<user>:<password>@tcp(<host>:<port>)/<database>")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"data": "Hello world"})
+	router := gin.Default()
+
+	router.GET("/users", func(c *gin.Context) {
+
+		rows, err := db.Query("SELECT * FROM users")
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		defer rows.Close()
+
+		var users []map[string]interface{}
+		for rows.Next() {
+			var id int
+			var email, name string
+			var power int
+			err = rows.Scan(&id, &email, &name, &power)
+			if err != nil {
+				c.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+			user := map[string]interface{}{
+				"id":    id,
+				"email": email,
+				"name":  name,
+				"power": power,
+			}
+			users = append(users, user)
+		}
+
+		c.JSON(http.StatusOK, gin.H{"users": users})
 	})
 
-	r.Run()
+	router.Run(":8080")
 }
